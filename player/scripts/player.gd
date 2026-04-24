a038de68bb5c336927b17402aa7bc0663ac59096
eachLetterMatters <daarnoox@gmail.com>
2026-04-24 16:59:42 +0200

    level fade transition added

--- player/scripts/player.gd
+++ player/scripts/player.gd
@@ -30,7 +30,11 @@ var gravity_multiplier : float = 1.0
 #endregion
 
 func _ready() -> void:
+	# quick fix for multiple players spawn
+	if get_tree().get_first_node_in_group("Player") != self:
+		self.queue_free()
 	initialize_states()
+	self.call_deferred("reparent", get_tree().root)
 	pass
 	
 func _unhandled_input(event: InputEvent) -> void:
